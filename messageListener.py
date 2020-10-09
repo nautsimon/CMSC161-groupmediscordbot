b302ce750b1703b39267d5d68d51e5e7994fa4d4
Simon mahns <ak.simonm@gmail.com>
2020-10-08 22:10:06 -0400

    *arg

--- messageListener.py
+++ messageListener.py
@@ -2,20 +2,31 @@ import discord
 import os
 import requests
 from discord.ext import commands
-DISCORD_TOKEN = "shhhhhhh"
+DISCORD_TOKEN = "shhhhh"
 url = 'https://api.groupme.com/v3/bots/post'
 bot = commands.Bot(command_prefix="$")
+
+
+def convertTuple(tup):
+    stringed = ' '.join(tup)
+    return stringed
+
+# Driver code
+
+
 @bot.command()
-async def send(ctx, arg):
-  if ctx.message.channel.id == "shhhhhhh" and str(arg) != "":
-    if str(ctx.author.display_name) != "None":
-      authName = str(ctx.author.display_name)
+async def send(ctx, *arg):
+    messBody = convertTuple(arg)
+    print(messBody)
+    if ctx.message.channel.id == "shhhhh" and str(messBody) != "":
+        if str(ctx.author.display_name) != "None":
+            authName = str(ctx.author.display_name)
+        else:
+            authName = str(ctx.author.name)
+        payload = str(messBody) + "  " + "~" + authName + " from Discord~"
+        myobj = {"text": payload, "bot_id": "shhhhh"}
+        requests.post(url, data=myobj)
     else:
-      authName =str(ctx.author.name)
-    payload = arg + "  "+ "~"+ authName + " from Discord~"
-    myobj = {"text" : payload, "bot_id" : "shhhhhhh"}
-    requests.post(url, data = myobj)
-  else:
-    print("wrong channel or empty message")
+        print("wrong channel or empty message")
 
 bot.run(DISCORD_TOKEN)
